020b390bb1bdba2205b3a08f914a318a3c1a6424
zhewei chen <1241770441@qq.com>
2023-03-11 19:44:35 +0800

    使用vuex(mapState)

--- src/components/CountCom.vue
+++ src/components/CountCom.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
-    <h1>当前求和为：{{ this.$store.state.sum }}</h1>
+    <h1>当前求和为：{{ sum }}</h1>
+    <h1>当前求和放大10倍后为：{{ bigsum }}</h1>
+    <h1>我在{{ school }}，学习{{ subject }}</h1>
     <select v-model="number">
       <option :value="1">1</option>
       <option :value="2">2</option>
       <option :value="3">3</option>
     </select>
     <button @click="add">+</button>
-    <!--    <button @click="minus">-</button>-->
-    <!--    <button @click="add1">当前求和为奇数在加</button>-->
-    <!--    <button @click="add2">等一等再加</button>-->
+    <button @click="minus">-</button>
+    <button @click="add1">当前求和为奇数在加</button>
+    <button @click="add2">等一等再加</button>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapState } from "vuex";
+
 export default {
   name: "CountCom",
   data() {
@@ -27,6 +31,23 @@ export default {
       // 通知store加
       this.$store.dispatch("add", this.number);
     },
+    minus() {
+      // 通知store减
+      this.$store.dispatch("minus", this.number);
+    },
+    add1() {
+      // 通知store加
+      this.$store.dispatch("add1", this.number);
+    },
+    add2() {
+      // 通知store加
+      this.$store.dispatch("add2", this.number);
+    },
+  },
+  computed: {
+    // 借助mapState生成计算属性，从state中读取数据。（数组写法）
+    ...mapState(["sum", "school", "subject"]),
+    ...mapGetters(["bigsum"]),
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -7,18 +7,39 @@ export default new Vuex.Store({
   state: {
     //countCom组件中的sum
     sum: 0,
+    school: "NEU",
+    subject: "Vue",
   },
-  getters: {},
+  getters: {
+    //countCom组件中的sum
+    bigsum: function (state) {
+      return state.sum * 10;
+    },
+  },
   mutations: {
     Add: function (state, value) {
-      console.log("value", value);
       state.sum += value;
-      console.log("state.sum", state.sum);
+    },
+    Minus: function (state, value) {
+      state.sum -= value;
     },
   },
   actions: {
     add: function (context, value) {
       context.commit("Add", value);
+    },
+    minus: function (context, value) {
+      context.commit("Minus", value);
+    },
+    add1: function (context, value) {
+      if (context.state.sum % 2) {
+        context.commit("Add", value);
+      }
+    },
+    add2: function (context, value) {
+      setTimeout(() => {
+        context.commit("Add", value);
+      }, 1000);
     },
   },
   modules: {},

cf246a94ead755c3bfa3a72d2ed5326c6b9b7289
zhewei chen <1241770441@qq.com>
2023-03-11 19:56:31 +0800

    使用vuex(mapActions, mapGetters, mapMutations, mapState)

--- src/components/CountCom.vue
+++ src/components/CountCom.vue
@@ -8,15 +8,15 @@
       <option :value="2">2</option>
       <option :value="3">3</option>
     </select>
-    <button @click="add">+</button>
-    <button @click="minus">-</button>
-    <button @click="add1">当前求和为奇数在加</button>
-    <button @click="add2">等一等再加</button>
+    <button @click="Add(number)">+</button>
+    <button @click="Minus(number)">-</button>
+    <button @click="add1(number)">当前求和为奇数在加</button>
+    <button @click="add2(number)">等一等再加</button>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapState } from "vuex";
+import { mapActions, mapGetters, mapMutations, mapState } from "vuex";
 
 export default {
   name: "CountCom",
@@ -27,25 +27,28 @@ export default {
     };
   },
   methods: {
-    add() {
-      // 通知store加
-      this.$store.dispatch("add", this.number);
+    //程序员亲自写方法
+    /* increment(){
+      this.$store.commit('JIA',this.n)
     },
-    minus() {
-      // 通知store减
-      this.$store.dispatch("minus", this.number);
-    },
-    add1() {
-      // 通知store加
-      this.$store.dispatch("add1", this.number);
-    },
-    add2() {
-      // 通知store加
-      this.$store.dispatch("add2", this.number);
-    },
+    decrement(){
+      this.$store.commit('JIAN',this.n)
+    }, */
+
+    //借助mapMutations生成对应的方法，方法中会调用commit去联系mutations(对象写法)
+    // ...mapMutations({increment:'JIA',decrement:'JIAN'}),
+
+    //借助mapMutations生成对应的方法，方法中会调用commit去联系mutations(数组写法)
+    // ...mapMutations(['JIA','JIAN']),
+
+    /* ************************************************* */
+    // 直接操作mutations
+    ...mapMutations(["Add", "Minus"]),
+    ...mapActions(["add1", "add2"]),
   },
   computed: {
     // 借助mapState生成计算属性，从state中读取数据。（数组写法）
+    // 若想要使用数组写法，生成的计算属性名必须和state中的属性名一致
     ...mapState(["sum", "school", "subject"]),
     ...mapGetters(["bigsum"]),
   },

67ef87a1920c528fec9da533c11ff7e861cc55e5
zhewei chen <1241770441@qq.com>
2023-03-13 03:18:44 +0800

    前置路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -7,28 +7,36 @@ const routes = [
   {
     path: "/",
     redirect: "/home",
+    meta: {
+      permission: false,
+    },
   },
   {
     path: "/about",
+    permission: false,
     component: () => import("@/views/AboutView.vue"),
   },
   {
     path: "/home",
     component: () => import("@/views/HomeView.vue"),
+    permission: false,
     // 子路由
     children: [
       {
         path: "news",
         component: () => import("@/views/NewsView.vue"),
+        permission: true,
       },
       {
         path: "message",
         component: () => import("@/views/MessageView.vue"),
+        permission: true,
         children: [
           {
             name: "xiangqing",
             path: "detail/",
             component: () => import("@/views/DetailView.vue"),
+            permission: true,
             // 开启props传参 第一种写法
             // props: true,
             // 开启props传参 第二种写法
@@ -50,5 +58,9 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
-
+router.beforeEach((to, from, next) => {
+  if (localStorage.getItem("school") === "atguigu") {
+    next();
+  }
+});
 export default router;
